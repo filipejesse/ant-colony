75e01c63d74d687e810fb5664ce29f4c4fbcc934
Filipe Jessé <filipejesse5@gmail.com>
2019-01-19 23:41:38 +0000

    Add Footer Component

---
+++ src/components/menu/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css" />
+      </div>
+    );
+  }
+}
+
+export default Footer;

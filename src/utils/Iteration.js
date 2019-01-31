e906accda0e9bce24691d5859e067ad7e26fc029
Filipe Jessé <filipejesse5@gmail.com>
2019-01-31 18:25:38 +0000

    Delete addFile and addNode in iteration .js

--- src/utils/Iteration.js
+++ src/utils/Iteration.js
@@ -1,15 +1,8 @@
 class Iteration {
 
-  addFile( {file} ) {
-    console.log(file);
-    if (file !== "" && !this.hasOwnProperty(file)){
-      this[file] = [];
     }
   }
 
-  addNode({ address }) {
-    if (address !== "" && !this.hasOwnProperty(address)) {
-      this[address] = Array(0);
     }
   }
 
